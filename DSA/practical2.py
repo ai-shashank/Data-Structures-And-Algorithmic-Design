1c22f270dcc1eb72cf2ab5867e8cd7f31aa66fdb
Shashank Sinha <shashank.sinha@rungta.org>
2026-03-17 14:54:28 +0530

    Delete DSA directory

--- DSA/practical2.py
+++
@@ -1,8 +0,0 @@
-# Search element in a list 
-lst = [5, 15, 25, 35, 45] 
-print("List:", lst) 
-key = int(input("Enter element to search: ")) 
-if key in lst: 
- print("Element found at index:", lst.index(key)) 
-else: 
- print("Element not found")
